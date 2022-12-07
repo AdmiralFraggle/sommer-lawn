0185936019991ff27d43093f5d5e4ab71b754881
Cayden Sommer <caydensommer@gmail.com>
2022-12-06 22:21:13 -0600

    sommer commit 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,8 +19,8 @@ export default defineConfig({
       external: ['svgo'],
     },
   },
-  site: 'https://hellotham.github.io',
-  base: '/hello-astro',
+  site: 'https://sommerlawn.com',
+  base: '',
   integrations: [tailwind(), sitemap(), image(), mdx(), alpinejs(), robotsTxt()],
   experimental: {
     integrations: true,
