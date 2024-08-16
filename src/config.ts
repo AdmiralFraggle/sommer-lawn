6362a587d3b04baf82882235a0afca3a4a90723c
Evan Harmon <evan@evanharmon.com>
2024-08-15 22:48:41 -0500

    Finish hero component

--- src/config.ts
+++ src/config.ts
@@ -30,7 +30,7 @@ export const SiteMetadata = {
 
 export { default as Logo } from './assets/svg/astro/astro-icon-dark.svg'
 export { default as LogoImage } from './assets/astro/astro-logo-dark.png'
-export { default as FeaturedSVG } from './assets/svg/undraw/undraw_design_inspiration.svg'
+export { default as FeaturedSVG } from './assets/Lawn.png'
 export { default as DefaultSVG } from './assets/svg/undraw/undraw_my_feed.svg'
 export { default as DefaultImage } from './assets/undraw/undraw_my_feed.png'
 
